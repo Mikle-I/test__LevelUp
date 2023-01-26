79f3789068f8aa64ba8a55553cdc7cc58bb06c63
Mikle-I <ivanovm2020@icloud.com>
2023-01-26 22:10:01 +0300

    first commit

---
+++ script/index.js
@@ -0,0 +1,6 @@
+$("document").ready(function () {
+  var options = { videoId: "SG65uAB1L2A", start: 3 };
+  $("#wrapper").tubular(options);
+  // f-UGhWj1xww cool sepia hd
+  // 49SKbS7Xwf4 beautiful barn sepia
+});
